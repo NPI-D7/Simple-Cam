64aa56e46288367c7848eda5aaeb029dcb246916
Tobi <tobid7@outlook.de>
2021-12-09 20:17:20 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -5,17 +5,17 @@ void Thread1(RenderD7::Parameter param) {
         RenderD7::Thread::sleep(1000 * 1); // wait; also, this is needed to allow for concurrency (refer to the documentation for m3d::Thread::sleep())
     }
 }
-rnd7::Clock clock;
+rnd7::Clock clocktr;
 int main()
 {
     RenderD7::Init::Main("Simple-Cam");
     //RenderD7::Thread th1(Thread1, 1);
     //th1.start();
-    clock.restart();
+    clocktr.restart();
     while (RenderD7::MainLoop())
     {
         RenderD7::OnScreen(Top);
-        RenderD7::DrawText(0, 0, 1.3f, RenderD7::Color::Hex("#FFFFFF"), std::to_string(clock.getCurrentTime()));
+        RenderD7::DrawText(0, 0, 1.3f, RenderD7::Color::Hex("#FFFFFF"), std::to_string(clocktr.getCurrentTime()));
         C3D_FrameEnd(0);
     }
    // th1.kill();
