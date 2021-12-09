ee5f6ef8ecb6bbc86527d9ba38d90ef10ec91c4e
Tobi <tobid7@outlook.de>
2021-12-09 21:11:10 +0100

    Update renderd7.cpp

--- extlibs/RenderD7/renderd7.cpp
+++ extlibs/RenderD7/renderd7.cpp
@@ -45,7 +45,7 @@ int mt_screen;
 //int mt_width = mt_screen ? 320 : 400;
 float mt_txtSize;
 bool metrikd = false;
-double mt_fpsgraph[320];
+//double mt_fpsgraph[320];
 //-------------------------------------------
 bool currentScreen = false;
 
@@ -250,7 +250,7 @@ void frameloop()
 		last_time = osGetTime();
 	}
 	d11framerate = current_fps;
-        mt_fpsgraph[320] = current_fps;
+        //mt_fpsgraph[320] = current_fps;
 }
 float getframerate()
 {
@@ -838,10 +838,10 @@ void RenderD7::DrawMetrikOvl()
 	RenderD7::DrawText(0, 0, mt_txtSize, mt_txtcolor, "FPS: " + RenderD7::GetFramerate());
         //RenderD7::DrawText(0, 50, mt_txtSize, mt_txtcolor, "CPU: " + std::to_string(C3D_GetProcessingTime()*6.0f) + "/" + std::to_string(C3D_GetProcessingTime()));
         //RenderD7::DrawText(0, 70, mt_txtSize, mt_txtcolor, "GPU: " + std::to_string(C3D_GetDrawingTime()*6.0f) + "/" + std::to_string(C3D_GetDrawingTime()));
-        for (int z = 0; z < 320; z++)
-        {
-             C2D_DrawLine(z, 239 - mt_fpsgraph[z], mt_txtcolor, z + 1, 239 - mt_fpsgraph[z + 1], mt_txtcolor, 1, 1);
-        }
+       // for (int z = 0; z < 320; z++)
+      //  {
+    //         C2D_DrawLine(z, 239 - mt_fpsgraph[z], mt_txtcolor, z + 1, 239 - mt_fpsgraph[z + 1], mt_txtcolor, 1, 1);
+     //   }
 }
 
 /*RenderD7::Console::Console()
