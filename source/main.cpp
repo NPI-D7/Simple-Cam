6ab83e085a74c8fdddbd45fe5f299378be0fc067
Tobi <tobid7@outlook.de>
2021-12-24 03:43:07 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,17 @@
 #include <renderd7.hpp>
 
-rnd7::Clock clocktr;
-rnd7::Time tr = clocktr.getCurrentTime();
+//rnd7::Clock clocktr;
+//rnd7::Time tr = clocktr.getCurrentTime();
 int main()
 {
     RenderD7::Init::Main("Simple-Cam");
     
-    clocktr.restart();
+   // clocktr.restart();
     while (RenderD7::MainLoop())
     {
         RenderD7::OnScreen(Top);
         RenderD7::DrawText(0, 30, 1.3f, RenderD7::Color::Hex("#FFFFFF"), "Hi");
-        RenderD7::DrawText(0, 0, 1.3f, RenderD7::Color::Hex("#FFFFFF"), std::to_string(tr.asSeconds()));
+      //  RenderD7::DrawText(0, 0, 1.3f, RenderD7::Color::Hex("#FFFFFF"), std::to_string(tr.asSeconds()));
         C3D_FrameEnd(0);
     }
     RenderD7::Exit::Main();
