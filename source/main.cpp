abaee88bf22fbf341c3a7ec907c77c26778aca2f
Tobi <tobid7@outlook.de>
2021-12-24 04:09:17 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -12,6 +12,8 @@ int main()
         RenderD7::OnScreen(Top);
         RenderD7::DrawText(0, 30, 1.3f, RenderD7::Color::Hex("#FFFFFF"), "Hi");
         RenderD7::DrawNFRect(30, 30, 170, 130, C2D_Color32(255, 255, 255, 255));
+        RenderD7::DrawNFRect(20, 30, 170, 130, C2D_Color32(255, 255, 255, 255));
+        RenderD7::DrawNFRect(50, 30, 300, 190, C2D_Color32(255, 255, 255, 255));
       //  RenderD7::DrawText(0, 0, 1.3f, RenderD7::Color::Hex("#FFFFFF"), std::to_string(tr.asSeconds()));
         C3D_FrameEnd(0);
     }
